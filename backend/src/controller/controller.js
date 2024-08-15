eebb05a26d27cfc444b9349eacfa2468ff6cd4bb
WescAlves <wesley.c.alves98@gmail.com>
2024-08-15 13:14:19 -0300

    "Finalizando front-end"

--- backend/src/controller/controller.js
+++ backend/src/controller/controller.js
@@ -7,6 +7,10 @@ const findAll = async (req, res) => {
 
 const addRestaurant = async (req, res) => {
     console.log(req.body)
+    if(req.body.localization.coordinates.lat!==null){
+        const arrayBody = [req.body.localization.coordinates.lat, req.body.localization.coordinates.lng]
+        req.body.localization.coordinates = arrayBody;
+    }
     const result = await restaurant.create(req.body)
     res.json(result);
 } 
--- frontend/food-finder/src/component/Map/index.js
+++ frontend/food-finder/src/component/Map/index.js
@@ -2,11 +2,12 @@ import { MapContainer, TileLayer, useMap, useMapEvents} from 'react-leaflet'
 import { Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
 import './style.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import L from 'leaflet';
-import marker from '../../assets/marker.icon.jpg';
+import marker from '../../assets/marcador.png';
 import { useRestaurantData } from '../../hooks/useRestaurantData';
 import { useRestaurantDataMutate } from '../../hooks/useRestaurantDataMutate';
+import axios from 'axios';
 
 
 const myIcon = new L.Icon({
@@ -20,24 +21,33 @@ const myIcon = new L.Icon({
 
 function Map() {
   const initialPosition = [-6.88817982014655, -38.55806053465703];
-  const [markerPosition, setMarkerPosition] = useState(initialPosition)
+  const [markerPosition, setMarkerPosition] = useState([0,0])
   const [inputValue, setInputValue] = useState('')
-
+  const [marcadorSelecionado, setMarcadorSelecionado] = useState(null);
   const {mutate} = useRestaurantDataMutate()
   const {data} = useRestaurantData()
 
+
+
   function GetAllRestaurantes() {
     console.log(data)
-    return data?.map((restaurant) => (
-      <Marker key={restaurant.id} position={restaurant.localization} icon={myIcon} />
-    ))
+    return data?.map((restaurant) => {
+      
+      return (
+        
+      <Marker key={restaurant.id} position={restaurant.localization.coordinates} icon={myIcon}>
+        <Popup>{restaurant.name}<br/> <br/> 
+        <button onClick={async () => {await deleteRestaurant(restaurant.name)}}>Deletar</button></Popup>
+      </Marker>
+      )
+  })
   }
   
   function salvarRestaurante() {
     const name = inputValue;
     const localization = {
       type:"Point",
-      coordinates: [markerPosition[0],markerPosition[1]]
+      coordinates: markerPosition
     }
     const restaurantData = {
       name,
@@ -46,38 +56,56 @@ function Map() {
     
     mutate(restaurantData)   
   }
+  
+  async function deleteRestaurant (name){
+    const result = await axios.delete(`http://localhost:4000/${name}`)
+    console.log(result)
+  }
 
   function updateInput (e){
     setInputValue(e.target.value)
   }
-   function SetMarker() {
+  
+  function SetMarker() {
     const map = useMapEvents({
       click(e) {
-        console.log('Disparou')
         const coordinates = e.latlng;
         setMarkerPosition(coordinates)
       },
     })
     return(
-      <Marker position={markerPosition} icon={myIcon} >
-        <Popup>You are here</Popup>
-      </Marker>
+      <Marker key={5} position={markerPosition} icon={myIcon}/>
     )
   }
 
     return (
       <>
-        <MapContainer center={markerPosition} zoom={13} scrollWheelZoom={true} className='map-container' >
+      <h1>Food Finder</h1>
+        <MapContainer center={initialPosition} zoom={13} scrollWheelZoom={true} className='map-container' >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {/* <GetAllRestaurantes/> */}
+          <GetAllRestaurantes/>
+          
           <SetMarker/>
         </MapContainer>
-        <input value={inputValue} onChange={updateInput} placeholder='Ex: Fast-Lanches'/>
-        <button onClick={salvarRestaurante}>Salvar</button>
-        <button onClick={GetAllRestaurantes}>Get</button>
+        <div className='form'>
+          <input value={inputValue} onChange={updateInput} placeholder='Ex: Fast-Lanches' className='input'/>
+          <button className='btn' onClick={salvarRestaurante}>Salvar</button>
+        
+        </div>
+        <div className='div'>
+          <ul className='lista'>
+            {data?.map((item) => {
+              return(
+                <li key={item.id}>
+                  {item.name}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </>
     );
 }
